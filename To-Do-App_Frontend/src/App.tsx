f9a8760f8c99eb9e2e2fbcc74de378abfb44c688
JamesImreNemeth <jamesnemeth1999@gmail.com>
2025-02-27 15:24:19 +1100

    CORS config has been made to allowed connect between the front and back end

--- To-Do-App_Frontend/src/App.tsx
+++ To-Do-App_Frontend/src/App.tsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    fetch("http://localhost:8080/todos")
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((e) => console.warn(e));
+  }, []);
+
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
