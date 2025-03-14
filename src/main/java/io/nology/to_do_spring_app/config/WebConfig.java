1b5fa25e7359daec524aa5dd0559ccdedf7fda69
JamesImreNemeth <jamesnemeth1999@gmail.com>
2025-03-14 12:30:31 +1100

    change from https to http

--- src/main/java/io/nology/to_do_spring_app/config/WebConfig.java
+++ src/main/java/io/nology/to_do_spring_app/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
         String[] allowedOrigins = {
                 "http://localhost:5173/",
                 "http://127.0.0.1:5173/",
-                "https://3.26.14.209",
+                "http://3.26.14.209",
                 "https://james-nemeth.github.io/"
         };
         registry.addMapping("/**")
